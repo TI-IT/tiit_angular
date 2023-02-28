24cee712a0e58bcbc170920c869bffb19e340a45
Tigran <go.projects.ogle@gmail.com>
2023-02-28 11:40:10 +0300

    lesson 13

---
+++ src/app/components/lesonsV/directive-lesson13/directive-lesson13.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-lesson13',
+  templateUrl: './directive-lesson13.component.html',
+  styleUrls: ['./directive-lesson13.component.scss']
+})
+export class DirectiveLesson13Component {
+
+}
