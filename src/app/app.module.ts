65a8c3d5a187bb4ad15074587797178c229ed88d
TIGRA <go.tiit.ogle@gmail.com>
2023-03-01 21:07:53 +0300

    lesson 20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ConnectionsComponent } from './components/documentation/connections/connections.component';
+import { PostComponent } from './components/projects/createComponents/post/post.component';
+import { PostFormComponent } from './components/projects/createComponents/post-form/post-form.component';
+import { AppPostComponent } from './components/projects/createComponents/app-post/app-post.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnectionsComponent,
+    PostComponent,
+    PostFormComponent,
+    AppPostComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/projects/createComponents/app-post/app-post.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+export interface Post {
+title: string,
+  text: string,
+  id?: number
+}
+@Component({
+  selector: 'app-app-post',
+  templateUrl: './app-post.component.html',
+  styleUrls: ['./app-post.component.scss']
+})
+export class AppPostComponent {
+  posts: Post[] = [
+  {title: 'Хочу выучить Angular Components', text: 'Я все еще учу компаненты', id: 1},
+  {title: 'Следующий блок будет про директивы', text: 'Будут директивы и еще пайпы', id: 2},
+  {title: 'Хочу выучить Angular', text: 'Angular компаненты', id: 3},
+]
+}
